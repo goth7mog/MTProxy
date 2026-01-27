0c611e20b34b0cda76ffdc6e19327a276e6b7398
Alexander <kuzurik797@gmail.com>
2026-01-27 23:51:11 +0000

    shadowsocks protocol added

--- main.tf
+++ main.tf
@@ -41,7 +41,6 @@ resource "digitalocean_droplet" "web" {
       - add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
       - apt-get update
       - apt-get install -y docker-ce
-      - sleep 5
       - usermod -aG docker ubuntu
       - curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
       - chmod +x /usr/local/bin/docker-compose
