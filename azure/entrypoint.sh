d394f4207a06072ceeabbee00f3f3f4d2f8885cc
Alexander <kuzurik797@gmail.com>
2026-02-15 03:09:52 +0000

    entrypoint.sh

---
+++ azure/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+# Configure ngrok if authtoken is provided
+if [ -n "$NGROK_AUTHTOKEN" ]; then
+    ngrok config add-authtoken "$NGROK_AUTHTOKEN"
+    ngrok tcp 8388 --log stdout &
+fi
+
+# Start dnscrypt-proxy
+dnscrypt-proxy -config /etc/dnscrypt-proxy.toml &
+
+# Give dnscrypt-proxy time to start
+sleep 2
+
+# Start shadowsocks server
+exec ss-server \
+    -s 0.0.0.0 \
+    -p 8388 \
+    -m "${SHADOWSOCKS_METHOD}" \
+    -k "${SHADOWSOCKS_PASSWORD}" \
+    -u \
+    -d 127.0.0.1:5053
